f498e9b0fd3ab7f81815b4fe3867745165c9dcf4
Eric Baldwin <Ericbaldwinn@gmail.com>
2022-03-22 20:43:45 -0700

    Implemented custom spotify hook

--- spotify-build/pages/api/auth/[...nextauth].js
+++ spotify-build/pages/api/auth/[...nextauth].js
@@ -9,7 +9,7 @@ async function refreshAccessToken(token) {
     spotifyApi.setRefreshToken(token.refreshToken);
 
     const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
-    console.log("Refreshed token is", refreshedToken);
+    //console.log("Refreshed token is", refreshedToken);
 
     return {
       ...token,
